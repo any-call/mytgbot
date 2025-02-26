1e4094ffd5356125e1e289898a616dede045c040
jinguihua <156711203@qq.com>
2025-02-26 16:38:09 +0800

    changed

---
+++ inline.message.go
@@ -0,0 +1,28 @@
+package mytgbot
+
+import (
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+type inLine struct {
+}
+
+func ImpInline() inLine {
+	return inLine{}
+}
+
+func (self inLine) SendQueryResultArticle(bot *tgbotapi.BotAPI, queryID string, configFn func(inlineCfg *tgbotapi.InlineQueryResultArticle)) (*tgbotapi.APIResponse, error) {
+	//返回查询的卡片
+	article := tgbotapi.NewInlineQueryResultArticle(queryID, "", "")
+	if configFn != nil {
+		configFn(&article)
+	}
+
+	inlineConfig := tgbotapi.InlineConfig{
+		InlineQueryID: queryID,
+		Results:       []interface{}{article},
+		CacheTime:     1,
+	}
+
+	return bot.Request(inlineConfig)
+}
